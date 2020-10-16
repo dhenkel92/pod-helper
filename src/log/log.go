915a03334bc20c4e6d67e2b2ee49ef74e074a1f7
Daniel Henkel <Daniel.henkel@westwing.de>
2020-10-16 12:28:42 +0200

    remove not needed gitignore entries

---
+++ src/log/log.go
@@ -0,0 +1,32 @@
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+)
+
+func init() {
+	Trace = log.New(ioutil.Discard,
+		"TRACE: ",
+		log.Ltime)
+
+	Info = log.New(os.Stdout,
+		"INFO: ",
+		log.Ltime)
+
+	Warning = log.New(os.Stdout,
+		"WARNING: ",
+		log.Ltime)
+
+	Error = log.New(os.Stderr,
+		"ERROR: ",
+		log.Ltime)
+}
