b517797056d9362391a8fe8c7e3a33e508da0187
Daniel Henkel <daniel@henkel.tech>
2020-10-17 14:28:00 +0200

    remove debug logs from config parsing

--- src/config/run_config.go
+++ src/config/run_config.go
@@ -1,16 +1,13 @@
 package config
 
 import (
-	"fmt"
 	"github.com/urfave/cli/v2"
 	"strings"
 )
 
 func newRunConfig(c *cli.Context) RunConfig {
 	command := c.String("command")
-
 	commands := strings.Split(command, " ")
-	fmt.Println(commands)
 
 	return RunConfig{Command: commands}
 }
